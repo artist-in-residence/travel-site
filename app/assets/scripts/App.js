16089a3fcbd4d08ab46abdb8d9c7ea21a4718be9
Mason <artistinresidencestudios@gmail.com>
2019-02-15 22:00:44 -0500

    ES6 addition

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,15 @@
-var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0.");
+  }
+}
 
 alert("ABC 123");
 
 var john = new Person("John Doe", "blue");
   john.greet();
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "red");
   jane.greet();
+  jane.payTaxes();
